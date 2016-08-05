6f703a166a944590c1d4643123cf5b89041e1bbb
sleroy <sylvain.leroy@tocea.com>
2016-08-05 14:43:41 +0200

    First implementation of the connector

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# komea-salesforce-connector
-Prototype of Salesforce Connector for the Komea dashboard (written in Node.JS)
+Informations utiles :
+
+- Créer un compte salesforce
+- Aller dans les paramètres de compte
+- Réinitialiser le token de sécurité
+- Pour s'authentifier utiliser email + (password + token de sécurité juxtaposés)
+
+Pour faire des rquêtes SOQL : https://workbench.developerforce.com/select.php
+L'api http://jsforce.github.io
---
+++ salesforce-connector.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+'use strict';
+const program = require('commander');
+const logger = require('winston');
+const jsforce = require('jsforce');
+//PU8ohuuoOJ7fuUHmKFQRH09z
+//login sylvain.leroy@metrixware.com
+//token WP-y@5YmsalesforcePU8ohuuoOJ7fuUHmKFQRH09z
+
+program
+    .version('1.0.0')
+    .option('-u, --username <login>', 'Provides the login to auth on salesforce : ex sl...@gmail.com')
+    .option('-k, --token <token>', 'Provides the token to loggerth on salesforce')
+    .option('-t, --test', 'Test the connection with salesforce')
+    .option('-r, --report <id>', 'Prints a report')
+    .option('-l, --report-list', 'Prints the list of reports')
+    .parse(process.argv);
+
+console.log('You have chose the action :');
+if (!program.username) {
+    throw new Error('--login is required');
+}
+if (!program.token) {
+    throw new Error('--token is required');
+}
+
+function connection(login, token, action) {
+    logger.info("Connection to Salesforce")
+    var conn = new jsforce.Connection();
+    conn.login(login, token, function(err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("Connection succeed");
+
+        action(conn);
+    });
+}
+
+if (program.test) {
+    logger.info("  - Testing connection with salesforce \nlogin %s\ntoken %s", program.username, program.token);
+    connection(program.username, program.token, function(conn) {});
+} else if (program.report) {
+    logger.info("  - Print a report with salesforce \nlogin %s\ntoken %s", program.username, program.token);
+
+    connection(program.username, program.token, function(conn) {
+
+        // Printing reports
+        conn.analytics.reports(function(err, reports) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("reports length: " + reports.length);
+            for (var i = 0; i < reports.length; i++) {
+                console.log(reports[i].id);
+                console.log(reports[i].name);
+            }
+        });
+        // get report reference
+        var reportId = program.report.id;
+        var report = conn.analytics.report(reportId);
+
+        // execute report synchronously
+        report.execute(function(err, result) {
+            if (err) {
+                return console.error("Cannot obtain the report " + err);
+            }
+            console.log(result.reportMetadata);
+            console.log(result.factMap);
+            // ...
+        });
+
+    });
+
+} else if (program.reportList) {
+    connection(program.username, program.token, function(conn) {
+        conn.query('SELECT Id, Name FROM Report', function(err, res) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log(JSON.stringify(res));
+            for (prop in res) {
+             console.log("Resultat " + prop);
+            }
+        });
+    });
+
+}
