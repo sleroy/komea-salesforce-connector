d34b5cb6698f72429cec275accb08e133bb883fb
sleroy <sylvain.leroy@tocea.com>
2016-08-12 16:01:41 +0200

    Moved sources

--- src/salesforce-connector.js
+++ src/salesforce-connector.js
@@ -8,11 +8,11 @@ const program = require('commander');
 const logger = require('winston');
 const jsforce = require('jsforce');
 
-
 program
     .version('1.0.0')
     .option('-u, --username <login>', 'Provides the login to auth on salesforce : ex sl...@gmail.com')
-    .option('-k, --token <token>', 'Provides the token to loggerth on salesforce')
+    .option('-p, --password <password>', 'Provides the account password')
+    .option('-k, --token <token>', 'Provides the token to sign in salesforce')
     .option('-t, --test', 'Test the connection with salesforce')
     .option('-r, --report <id>', 'Prints a report')
     .option('-l, --report-list', 'Prints the list of reports')
@@ -26,10 +26,14 @@ if (!program.token) {
     throw new Error('--token is required');
 }
 
-function connection(login, token, action) {
-    logger.info("Connection to Salesforce")
+if (!program.password) {
+    throw new Error('--password is required');
+}
+
+function connection(login, password, token, action) {
+    logger.info("Connection to Salesforce\n\tUser : %s\n\tToken %s\n\tPassword %s", login, token, password)
     var conn = new jsforce.Connection();
-    conn.login(login, token, function(err, res) {
+    conn.login(login, password+token, function(err, res) {
         if (err) {
             return console.error(err);
         }
@@ -40,12 +44,11 @@ function connection(login, token, action) {
 }
 
 if (program.test) {
-    logger.info("  - Testing connection with salesforce \nlogin %s\ntoken %s", program.username, program.token);
-    connection(program.username, program.token, function(conn) {});
+    connection(program.username, program.password, program.token, function(conn) {});
 } else if (program.report) {
     logger.info("  - Print a report with salesforce \nlogin %s\ntoken %s", program.username, program.token);
 
-    connection(program.username, program.token, function(conn) {
+    connection(program.username, program.password, program.token, function(conn) {
 
         // Printing reports
         conn.analytics.reports(function(err, reports) {
@@ -75,14 +78,18 @@ if (program.test) {
     });
 
 } else if (program.reportList) {
-    connection(program.username, program.token, function(conn) {
+    connection(program.username, program.password, program.token, function(conn) {
         conn.query('SELECT Id, Name FROM Report', function(err, res) {
             if (err) {
                 return console.error(err);
             }
-            console.log(JSON.stringify(res));
-            for (prop in res) {
-             console.log("Resultat " + prop);
+            logger.debug(JSON.stringify(res));
+            logger.info("Liste des rapports sur le compte Salesforce");
+            logger.info("--------------------------------------------");
+            for (var i=0, ni = res.records.length; i < ni ; ++i) {
+                var record = res.records[i];
+                logger.debug("record :" + JSON.stringify(record));
+                logger.info("%s : Rapport %s", record.Id, record.Name);
             }
         });
     });
